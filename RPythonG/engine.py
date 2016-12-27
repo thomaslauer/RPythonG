d52700c33b7c2ea14c6d934cc0c5631e877f9762
Thomas Lauer <thomaslauer01@gmail.com>
2016-12-27 14:18:55 -0600

    Can control avatar movement through directional arrow keys

--- RPythonG/engine.py
+++ RPythonG/engine.py
@@ -7,7 +7,17 @@ Surface.blit(source, dest, area=None, special_flags = 0): return Rect
 draw one image onto another
 '''
 
+
+# distance moved for a single press of the corresponding arrow key
+MOVE_RIGHT = 5
+MOVE_LEFT = -5
+MOVE_DOWN = 5
+MOVE_UP = -5
+
+
+
 class GameEngine:
+
 	def __init__(self):
 		self.initWindow(300, 300)
 		self.running = False
@@ -15,6 +25,20 @@ class GameEngine:
 		self.fps = 30
 		self.avatar = Avatar("Sci-fi Avatar.png", 15, 15)
 
+	# Get the currently pressed direction key(s), and return the direction to move accordingly
+	# return as a tuple
+	def getUserMove(self):
+		coords = [0,0]
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_UP]:
+			coords[1] += MOVE_UP
+		if keys[pygame.K_DOWN]:
+			coords[1] += MOVE_DOWN
+		if keys[pygame.K_LEFT]:
+			coords[0] += MOVE_LEFT
+		if keys[pygame.K_RIGHT]:
+			coords[0] += MOVE_RIGHT
+		return tuple(coords)
 
 
 	def initWindow(self, width, height):
@@ -25,12 +49,15 @@ class GameEngine:
 	def mainLoop(self):
 		self.running = True
 
-		x = 10;
+		x = 10
+		y = 10
 		while self.running:
 			self.window.fill(self.background_color)
-
-			self.avatar.drawSelf(self.window, x, 0)
-			x += 1
+			self.avatar.drawSelf(self.window, x, y)
+			
+			delta = self.getUserMove()
+			x += delta[0]
+			y += delta[1]
 
 			# check for exit
 			for event in pygame.event.get():
@@ -40,9 +67,13 @@ class GameEngine:
 				self.flipWindowBuffers()
 			
 			self.clock.tick(self.fps) # fps cap
+	
+	
+	
+
 	def flipWindowBuffers(self):
 		pygame.display.flip()
 
+
 	def setBackgroundColor(self, color):
 		self.background_color = color
-
