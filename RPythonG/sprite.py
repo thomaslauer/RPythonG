30c427743b1a1b4c02a0f1599e1ed03f58b0ecc8
Thomas Lauer <thomaslauer01@gmail.com>
2016-12-26 21:02:35 -0600

    Added Sprite

---
+++ RPythonG/sprite.py
@@ -0,0 +1,13 @@
+class sprite:
+	def __init__(self, image, id, sizex, sizey):
+		self.image = image
+		self.id = id
+		self.x = 0
+		self.y = 0
+		self.sizex = sizex
+		self.sizey = sizey
+
+	def setPosition(x, y):
+		self.x = x
+		self.y = y
+
